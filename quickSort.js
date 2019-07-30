6f5c3ce372f22d6c2906ddcde94a9ac4b46f6e3e
chenhao03 <chenhao03@qiyi.com>
2019-07-30 14:34:26 +0800

    1

--- quickSort.js
+++ quickSort.js
@@ -32,3 +32,23 @@ function quickSort(arr){
 
 let arr = quickSort([1,3,5,8,0,-1,9])
 console.log(arr);
+
+/*利用快排球topk*/
+function topk(arr, k, start, end){
+
+    if(arr.length <= 1 || arr.length < k) return ;
+    let temp = arr[start];
+    let left = [], right = [];
+    for(let i = start + 1; i < end; i++){
+        if(arr[i] <= temp) left.push(arr[i]);
+        else right.push(arr[i])
+    }
+    if(left.length == k - 1) return temp;
+    else if(left.length < k){
+        return topk(right, k - left.length - 1, 0, right.length);
+    }
+    else{
+        return topk(left, k, 0, left.length )
+    }
+
+}
