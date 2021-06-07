722dc159d7867264094d645c7a36b72c0565cdc0
chenhao278 <chenhao278@jd.com>
2021-06-07 09:51:19 +0800

    旋转数组中位数

---
+++ findNumInRotateArr.js
@@ -0,0 +1,28 @@
+/**
+ * 按照某个位置旋转的数组（连续且有序）求中位数，例如[7,8,9,1,2,3,4,5,6]，复杂度，如何降到比n更低
+ */
+function findRotateArr(arr){
+
+    if(!arr.length) return
+    let left = 0, right = arr.length - 1, mid
+    while(left < right){
+        mid = Math.floor((right + left)/2)
+        if(arr[right] > arr[mid]){
+            right = mid
+        }
+        else if(arr[right] < arr[mid]){
+            left = mid + 1
+        }
+        else{
+            right--
+        }
+    }
+    let size = arr.length
+    if(size % 2 == 1){
+        return arr[(let + Math.floor(size/2)%size)]
+    }
+    else{
+        return (arr[(left + Math.floor(size/2))%size] + arr[(left + Math.floor((size - 1)/2)%size)/2])
+    }
+
+}
