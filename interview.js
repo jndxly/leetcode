15c3e6830babf4e85210c8217491818088e59ff1
chenhao278 <chenhao278@jd.com>
2021-07-14 14:29:18 +0800

    symbol.iterator

--- interview.js
+++ interview.js
@@ -61,3 +61,23 @@ let a = {
         }
     })()
 }
+
+let obj = {
+    data: [ 'hello', 'world' ],
+    [Symbol.iterator]() {
+        const self = this;
+        let index = 0;
+        return {
+            next() {
+                if (index < self.data.length) {
+                    return {
+                        value: self.data[index++],
+                        done: false
+                    };
+                } else {
+                    return { value: undefined, done: true };
+                }
+            }
+        };
+    }
+};
